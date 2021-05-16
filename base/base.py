256c06fa741fbbb1c2dca9c2bb174fa84e9ba5c0
Juan Ibarra <jibarra@nahualit.com>
2021-05-16 20:50:14 -0300

    added fix variable

--- base/base.py
+++ base/base.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-EXPLICIT_WAIT = 30
+IMPLICIT_WAIT = 30
 
 
 class Base:
